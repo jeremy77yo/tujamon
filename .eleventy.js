552e3047f8b567099a880141a342aa83dc60d4b3
Jeremy Roberts <jeremy77yo@gmail.com>
2026-01-05 21:21:54 -0800

    Fix favicon passthrough in Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
   // Serve static files
   eleventyConfig.addPassthroughCopy({ "src/style.css": "style.css" });
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/favicon-16x16.png": "favicon-16x16.png" });
+  eleventyConfig.addPassthroughCopy({ "src/favicon-32x32.png": "favicon-32x32.png" });
+  eleventyConfig.addPassthroughCopy({ "src/apple-touch-icon.png": "apple-touch-icon.png" });
+  eleventyConfig.addPassthroughCopy({ "src/og-image.jpg": "og-image.jpg" });
 
   // ✅ Create an "artworks" collection from all markdown files in src/artworks
   eleventyConfig.addCollection("artworks", (collectionApi) => {
